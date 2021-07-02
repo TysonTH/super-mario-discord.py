e8721dd9116c6be27bb2a56bf63c156143ea9415
TysonTH <tysonth535+github@gmail.com>
2021-07-02 16:12:04 -0400

    added commands and config variables
    + added ip command (commented by default)
    + added run command to execute server script remotely

--- bot.py
+++ bot.py
@@ -1,11 +1,17 @@
-import os
 import discord
 import logging
+import os
+import os.path
 import subprocess
+import time
+import urllib.request
 from discord.ext import commands
+from discord.ext.commands import cooldown, BucketType
 from dotenv import load_dotenv
+from os import path
 from subprocess import call
 
+#Log information
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -13,7 +19,7 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 #Functions
-
+	
 def log(text):
 	print('[General]: ' + text)
 
@@ -21,6 +27,12 @@ def log(text):
 load_dotenv()
 token = os.getenv('token')
 guild = os.getenv('guild')
+
+#Open Ports
+minecraftPort = os.getenv('minecraftPort')
+
+#Scripts
+minecraftServer = os.getenv('minecraftServer')
 
 #Sounds
 hello = os.getenv('hello')
@@ -67,29 +79,76 @@ async def on_message(message):
 #async def on_command_error(error, ctx):
 	#await error.send('An error has occurred.')
 
-#Commands		
+#Commands
 
-	#General
+	# *General*
 	
-@client.command()
+@client.command(brief = 'Outputs the latency between Discord and the bot')
 async def ping(ctx):
 	await ctx.send(f'Pong! `{round(client.latency * 1000)}ms`')
 	
 
-	#Server
+	# *Server*
 	
-#@client.command(aliases = ['boot', 'start'])
-#async def run(ctx, *, game):
+#Please check /scripts and .env and add your directories, otherwise this will break
+@client.command(aliases = ['boot', 'start'], brief = 'Initializes a server for compatible games', description = 'Initializes a server for compatible games, type $run usage for more info.')
+@commands.cooldown(1.0, 30.0, BucketType.guild)
+async def run(ctx, *, game):
 	
-	#if game == 'minecraft':
-		#await ctx.send('The Minecraft server script has been executed, please wait a moment as the server initializes.')
-		#subprocess.call(minecraftServer, shell = True)
+	if game == 'usage':
+		await ctx.send('```$run [game], Example: $run minecraft\n\n' +
+					   'List of compatible games:\n' +
+					   '• Minecraft```')
+	
+	#Run a server if .running does not exist
+	elif path.exists('.running') == False:
+	
+		if game == 'minecraft' or game == 'mc':
+			await ctx.send('The Minecraft server script has been executed, please wait a moment as the server initializes.')
+			subprocess.call(minecraftServer, shell = True)
+			log('A Minecraft server has been initialized.')
+			
+            #Start 30 second timer to inform server should now be in service
+			time.sleep(30)
+			await ctx.send('The Minecraft server should now be up and running!')
+	
+	#Otherwise, inform the user a server cannot be executed and give further instructions.
+	else:
+		await ctx.send('A server is already running! Please contact a server administrator to request a restart or termination of the current session.')
+		
+#@client.command(aliases = ['address'], brief = 'Displays the server\'s external IP and open ports')
+#@commands.cooldown(1.0, 30.0, BucketType.guild)
+#async def ip(ctx):
+	
+	#Contact ident.me and store IP address as string
+	#extIP = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+
+	#await ctx.send("Server IP: `" + extIP + "`" +
+				   #"\nOpen Ports:" +
+				   #"\n```Minecraft: " + minecraftPort + "```")
+	
 		
 #Error Handlers
 
-#@run.error
-#async def run_error(error, ctx):
-	#await error.send("You must specify a game!")
+@run.error
+async def runError(ctx, error):
+	
+	#Check if currently on cooldown
+	if isinstance(error, commands.CommandOnCooldown):
+		await ctx.send('This command has a 30 second cooldown. You may use it again in `{:.2f}`s'.format(error.retry_after))
+	
+	else:
+		await ctx.send("You must specify a game!")
+		
+#@ip.error
+#async def ipError(ctx, error):
+	
+	#Check if currently on cooldown
+	#if isinstance(error, commands.CommandOnCooldown):
+		#await ctx.send('This command has a 30 second cooldown. You may use it again in `{:.2f}`s'.format(error.retry_after))
+	
+	#else:
+		#await ctx.send("An unknown error has occurred.")
 			
 #Run bot with corresponding token
 client.run(token)
