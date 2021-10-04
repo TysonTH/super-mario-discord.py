5c75bb83327dc58479f9bbddda88345b140fc3ac
TysonTH <tysonth535+github@gmail.com>
2021-10-04 15:51:37 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import asyncio
+import botConfig
 import discord
 import logging
 import os
@@ -7,7 +9,6 @@ import time
 import urllib.request
 from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType
-from dotenv import load_dotenv
 from os import path
 from subprocess import call
 
@@ -23,22 +24,25 @@ logger.addHandler(handler)
 def log(text):
 	print('[General]: ' + text)
 
-#Load Bot Token and Guild information
-load_dotenv()
-token = os.getenv('token')
-guild = os.getenv('guild')
+#Bot Config
+token = botConfig.token
+guild = botConfig.guild
+prefix = botConfig.prefix
+
+#Links
+botRepo = botConfig.botRepo
 
 #Open Ports
-minecraftPort = os.getenv('minecraftPort')
+minecraftPort = botConfig.minecraftPort
 
 #Scripts
-minecraftServer = os.getenv('minecraftServer')
+minecraftServer = botConfig.minecraftServer
 
 #Sounds
-hello = os.getenv('hello')
+hello = botConfig.helloSound
 
 #Initialize bot
-client = commands.Bot(command_prefix='$')
+client = commands.Bot(command_prefix=prefix)
 
 #Initialize extensions
 extensions = (
@@ -72,7 +76,10 @@ async def on_message(message):
 			
 	if message.content == 'hello mario':
 		await message.channel.send('Hello!')
-		#await message.channel.send(file=discord.File(hello))
+		try:
+			await message.channel.send(file=discord.File(hello))
+		except Exception as e:
+			log('User has found an easter egg but no sound file was provided!')
 		#Easter Egg
 		
 #@client.event
@@ -83,6 +90,10 @@ async def on_message(message):
 
 	# *General*
 	
+@client.command(aliases = ['github', 'repo', 'repository', 'source', 'download'], brief = 'Links to the current repository for this bot!')
+async def codebase(ctx):
+	await ctx.send(botRepo)
+	
 @client.command(brief = 'Outputs the latency between Discord and the bot')
 async def ping(ctx):
 	await ctx.send(f'Pong! `{round(client.latency * 1000)}ms`')
@@ -90,13 +101,13 @@ async def ping(ctx):
 
 	# *Server*
 	
-#Please check /scripts and .env and add your directories, otherwise this will break
-@client.command(aliases = ['boot', 'start'], brief = 'Initializes a server for compatible games', description = 'Initializes a server for compatible games, type $run usage for more info.')
+#Please check /scripts and configure .env and add your directories, otherwise this will not work
+@client.command(aliases = ['boot', 'start'], brief = 'Initializes a server for compatible games', description = 'Initializes a server for compatible games, type {0}run usage for more info.'.format(prefix))
 @commands.cooldown(1.0, 30.0, BucketType.guild)
 async def run(ctx, *, game):
 	
 	if game == 'usage':
-		await ctx.send('```$run [game], Example: $run minecraft\n\n' +
+		await ctx.send('```{0}run [game], Example: $run minecraft\n\n'.format(prefix) +
 					   'List of compatible games:\n' +
 					   '• Minecraft```')
 	
@@ -104,29 +115,76 @@ async def run(ctx, *, game):
 	elif path.exists('.running') == False:
 	
 		if game == 'minecraft' or game == 'mc':
-			await ctx.send('The Minecraft server script has been executed, please wait a moment as the server initializes.')
-			subprocess.call(minecraftServer, shell = True)
-			log('A Minecraft server has been initialized.')
+			try:
+				await ctx.send('The Minecraft server script has been executed, please wait a moment as the server initializes.')
+				subprocess.call(minecraftServer, shell = True)
+				log('A Minecraft server has been initialized.')
+				
+			except Exception as e:
+				await ctx.send('Could not successfully initialize the server, please contact bot administrator.')
+				log('A Minecraft server could not be initialized. Please check /scripts/runMinecraft.sh to make sure everything is set correctly. You must also ensure the script has execute permissions.')
 			
-            #Start 30 second timer to inform server should now be in service
-			time.sleep(30)
+            #Start 50 second timer to inform server should now be in service
+			time.sleep(50)
 			await ctx.send('The Minecraft server should now be up and running!')
 	
 	#Otherwise, inform the user a server cannot be executed and give further instructions.
 	else:
 		await ctx.send('A server is already running! Please contact a server administrator to request a restart or termination of the current session.')
 		
-#@client.command(aliases = ['address'], brief = 'Displays the server\'s external IP and open ports')
-#@commands.cooldown(1.0, 30.0, BucketType.guild)
-#async def ip(ctx):
+@client.command(aliases = ['address'], brief = 'Displays the server\'s external IP and open ports')
+@commands.cooldown(1.0, 30.0, BucketType.guild)
+async def ip(ctx):
 	
-	#Contact ident.me and store IP address as string
-	#extIP = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+	#Check if command is allowed by bot Administrator
+	if botConfig.ipCmdAllowed == True:
+		#Contact URL stored in botConfig and store IP address as string
+		extIP = urllib.request.urlopen(botConfig.ipTest).read().decode('utf8')
+		await ctx.send("Server IP: `" + extIP + "`" +
+					   "\nOpen Ports:" +
+					   "\n```Minecraft: " + minecraftPort + "```")
+	
+	else:
+		await ctx.send('Bot administrator has not authorized this command.')
+		log('IP grab attempt blocked. To change this behavior open botConfig.py and find the line: ' +
+			'\'ipCmdAllowed = False\'' +
+			'\nand replace False with True')
 
-	#await ctx.send("Server IP: `" + extIP + "`" +
-				   #"\nOpen Ports:" +
-				   #"\n```Minecraft: " + minecraftPort + "```")
+@client.command(aliases = ['bandwidth'], brief = 'Perform a speedtest, powered by Ookla™')
+@commands.cooldown(1.0, 90.0, BucketType.guild)		
+async def speedtest(ctx):
 	
+	#Check if command is allowed by bot Administrator
+	if botConfig.speedtestCmdAllowed == True:
+		import speedtest
+		
+		log('A user has just initiated a speedtest.')
+		await ctx.send('Attempting to perform Speedtest...')
+		
+		#https://github.com/sivel/speedtest-cli/wiki
+		
+		servers = []
+		threads = None
+		
+		attempt = speedtest.Speedtest()
+		attempt.get_servers(servers)
+		attempt.get_best_server()
+		
+		await ctx.send('Performing download test...')
+		attempt.download(threads=threads)
+		
+		await ctx.send('Performing upload test...')
+		attempt.upload(threads=threads)
+		
+		log('Here are the results: ' + attempt.results.share())
+		await ctx.send(attempt.results.share())
+		
+	else:
+		await ctx.send('Bot administrator has not authorized this command.')
+		log('Speedtest attempt blocked. To change this behavior open botConfig.py and find the line: ' +
+			'\'speedtestCmdAllowed = False\'' +
+			'\nand replace False with True')
+		
 		
 #Error Handlers
 
@@ -140,15 +198,25 @@ async def runError(ctx, error):
 	else:
 		await ctx.send("You must specify a game!")
 		
-#@ip.error
-#async def ipError(ctx, error):
+@ip.error
+async def ipError(ctx, error):
 	
 	#Check if currently on cooldown
-	#if isinstance(error, commands.CommandOnCooldown):
-		#await ctx.send('This command has a 30 second cooldown. You may use it again in `{:.2f}`s'.format(error.retry_after))
+	if isinstance(error, commands.CommandOnCooldown):
+		await ctx.send('This command has a 30 second cooldown. You may use it again in `{:.2f}`s'.format(error.retry_after))
 	
-	#else:
-		#await ctx.send("An unknown error has occurred.")
+	else:
+		await ctx.send("An unknown error has occurred.")
+		
+@speedtest.error
+async def speedtestError(ctx, error):
+	
+	#Check if currently on cooldown
+	if isinstance(error, commands.CommandOnCooldown):
+		await ctx.send('This command has a 90 second cooldown. You may use it again in `{:.2f}`s'.format(error.retry_after))
+	
+	else:
+		await ctx.send("An unknown error has occurred.")
 			
 #Run bot with corresponding token
 client.run(token)
