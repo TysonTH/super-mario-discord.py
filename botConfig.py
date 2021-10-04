7153d9f2af5d10a55b12ad672927eb781e513242
TysonTH <tysonth535+github@gmail.com>
2021-10-04 15:57:51 -0400

    New configuration system

---
+++ botConfig.py
@@ -0,0 +1,26 @@
+#Bot Information
+
+token = ''
+guild = ''
+prefix = '$'
+
+#Commands Permissions
+ipCmdAllowed = False
+speedtestCmdAllowed = False
+
+#Links
+botRepo = 'https://github.com/TysonTH/super-mario'
+ipTest = 'https://ident.me'
+
+#Ports
+
+minecraftPort = 'Unconfigured'
+
+
+#Scripts
+
+minecraftServer = 'scripts/runMinecraft.sh'
+
+#Sounds
+
+helloSound = 'sounds/hello.wav'
---
+++ minecraftConfig.py
@@ -0,0 +1,19 @@
+#Channels
+
+serverStatus = 'Insert channel ID for where bot will make announcements'
+
+#Directories
+
+coordinatesDirectory = 'cogs/minecraft/coordinates/'
+
+#Open Ports
+minecraftPort = 25565
+minecraftQueryPort = 25575
+
+#Scripts
+
+minecraftServer = 'scripts/runMinecraft.sh'
+
+#Links
+
+modsDirectory = 'Bot administrator has not added a URL for this.'
